60aba571d7bcebc94a365c0c6d80b5b9e535dc16
DaveYesland <david.yesland@rhinosecuritylabs.com>
2022-11-30 09:24:00 -0800

    Update regions

--- IPRotate.py
+++ IPRotate.py
@@ -19,11 +19,13 @@ ENABLED = 'ENABLED'
 DISABLED = 'DISABLED'
 STAGE_NAME = 'burpendpoint'
 API_NAME = 'BurpAPI'
-AVAIL_REGIONS = [
-	"us-east-1","us-west-1","us-east-2",
-	"us-west-2","eu-central-1","eu-west-1",
-	"eu-west-2","eu-west-3","sa-east-1","eu-north-1"
-]
+AVAIL_REGIONS = ['af-south-1', 'ap-south-1', 'eu-north-1', 
+				'eu-west-3', 'eu-south-1', 'eu-west-2', 
+				'eu-west-1', 'ap-northeast-3', 'ap-northeast-2', 
+				'me-south-1', 'ap-northeast-1', 'ca-central-1', 
+				'sa-east-1', 'ap-east-1', 'ap-southeast-1', 
+				'ap-southeast-2', 'eu-central-1', 'us-east-1', 
+				'us-east-2', 'us-west-1', 'us-west-2']
 
 class BurpExtender(IBurpExtender, IExtensionStateListener, ITab, IHttpListener):
 	def __init__(self):
